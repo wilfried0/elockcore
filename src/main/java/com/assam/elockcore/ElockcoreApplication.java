f95f85cad1c35903567ef54853eb1048822aee9d
wilfried0 <negprodattitude@gmail.com>
2022-05-04 13:27:07 +0100

    initialization of ms

---
+++ src/main/java/com/assam/elockcore/ElockcoreApplication.java
@@ -0,0 +1,13 @@
+package com.assam.elockcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElockcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElockcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assam/elockcore/ElockcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assam.elockcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElockcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
